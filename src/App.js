998e695f1dd1df65aad927ad9f6a65791806de40
Umair <umair@tezrosolutions.com>
2018-09-27 15:48:39 -0400

    Updates for token

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import DealsGrid from './components/DealsGrid/DealsGrid';
-import logo from './logo.svg';
 import './App.css';
+
+import Main from "./Main.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container-fluid">
-          <DealsGrid />
+        <header className="header-container">
+          <div className="container-fluid">
+          </div>
+        </header>
+        <div className="content-container">
+          <div className="container-fluid">
+            <Main />
+          </div>
         </div>
+        <footer className="footer-container">
+          <div className="container-fluid">
+          </div>
+        </footer>
       </div>
     );
   }
